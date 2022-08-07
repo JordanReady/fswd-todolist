c715c90a36f84be61c572177fcfbea9264df5ecb
JordanReady <jready112@gmail.com>
2022-08-06 21:33:03 -0500

    working todolist with pg in production

---
+++ app/javascript/src/index.js
@@ -0,0 +1,60 @@
+import $ from 'jquery';
+
+import {
+    indexTasks,
+    postTask,
+    deleteTask,
+    markTaskComplete,
+    markTaskActive
+  } from "./requests.js";
+  indexTasks();
+  
+
+  indexTasks(function (response) {
+    var htmlString = response.tasks.map(function(task) {
+      return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'>" + 
+      "<input type='checkbox' class='mark-complete mr-2' data-id='" + task.id + "'" + (task.completed ? "checked" : "") + ">" +
+      "<p class='task-content d-inline'>" + task.content + "</p>" +
+      "<button class='delete float-right btn btn-outline-danger' data-id='" + task.id + "'>Delete</button>" +
+    "</div>";
+    });
+    $('#tasks').html(htmlString);
+  });
+
+  var displayAllTasks = function (response) {
+    var htmlString = response.tasks.map(function(task) {
+      return "<div class='col-12 mb-3 p-2 border rounded task'>" + 
+        "<input type='checkbox' class='mark-complete mr-2' data-id='" + task.id + "'" + (task.completed ? "checked" : "") + ">" +
+        "<p class='task-content d-inline'>" + task.content + "</p>" +
+        "<button class='delete float-right btn btn-outline-danger' data-id='" + task.id + "'>Delete</button>" +
+      "</div>";
+    });
+  
+    $("#tasks").html(htmlString);
+  };
+
+  $(document).on("turbolinks:load", function () {
+    if ($('.static_pages.index').length > 0) {
+      indexTasks(displayAllTasks);
+    }
+  });
+  
+  $(document).on('submit', '#create-task', function (e) {
+    e.preventDefault();
+    postTask($('#new-task-content').val(), function () {
+      $('#new-task-content').val('');
+      indexTasks(displayAllTasks);
+    });
+  });
+  
+  $(document).on('click', '.delete', function () {
+    deleteTask($(this).data('id'), setTimeout(function() { indexTasks(displayAllTasks); }, 100));
+  });
+  
+  $(document).on('change', '.mark-complete', function () {
+    if (this.checked) {
+      markTaskComplete($(this).data('id'));
+    } else {
+      markTaskActive($(this).data('id'));
+    }
+  });
